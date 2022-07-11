6df8aa9ba04069cecfc3be2edfe3ec4e978bb8fc
10EastSea <xgf6580@gmail.com>
2022-07-11 21:37:55 +0900

    섬 연결하기 (미완2)

--- greedy/42861.py
+++ greedy/42861.py
@@ -2,10 +2,20 @@
 def solution(n, costs):
     answer = 0
     
-    graph = [[0 for _ in range(n)] for _ in range(n)]
+    nodes = [i for i in range(n)]
+    costs = sorted(costs, key=lambda x: x[2])
+    print(nodes); print(costs)
+    
     for n1, n2, c in costs:
-        graph[n1][n2] = c
-        graph[n2][n1] = c
-    print(graph)
+        if n1 > n2: tmp = n2; n2 = n1; n1 = tmp
+        
+        p_n1, p_n2 = nodes[n1], nodes[n2]
+        while p_n1 != p_n2:
+            if p_n2 == nodes[p_n2]: break
+            p_n2 = nodes[p_n2]
+        if p_n1 != p_n2: answer += c
+        
+        if p_n1 < nodes[n2]: nodes[nodes[n2]] = p_n1 # 기존에 부모노드가 새로 설정될 부모노드보다 크다면, 기존 노드도 새로운 부모 노드로 세팅
+        nodes[n2] = p_n1
     
     return answer
